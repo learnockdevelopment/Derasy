c1d1c253a000a146257149edf637dea160f65d8d
learnockdevelopment <learnockdevelopment@gmail.com>
2025-07-21 19:20:25 +0300

    fix: Update footer links to reflect correct names and URLs

--- full-kit/src/components/layout/footer.tsx
+++ full-kit/src/components/layout/footer.tsx
@@ -16,19 +16,19 @@ export function Footer() {
             rel="noopener noreferrer"
             className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
           >
-            Shadboard
+            Derasy
           </a>
           .
         </p>
         <p className="text-xs text-muted-foreground md:text-sm">
           Designed & Developed by{" "}
           <a
-            href="https://github.com/Qualiora"
+            href="https://learnock.com"
             target="_blank"
             rel="noopener noreferrer"
             className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
           >
-            Qualiora
+            Learnock
           </a>
           .
         </p>
